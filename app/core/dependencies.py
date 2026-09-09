9052cdfcde7b003cbe445e9468fb48c26bd94830
Ömer Faruk <omersoftware853@gmail.com>
2026-09-09 16:57:28 +0300

    fix(auth): disable auto refresh token and isolate supabase client

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -1,14 +1,13 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from supabase import create_client, ClientOptions
 from app.core.supabase import supabase
+from app.core.config import settings
 
 security = HTTPBearer()
 
-def get_current_user(
-        credentials: HTTPAuthorizationCredentials = Depends(security)
-        ):
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     token = credentials.credentials
-
     try:
         user_response = supabase.auth.get_user(token)
 
@@ -16,9 +15,9 @@ def get_current_user(
             raise HTTPException(
                 status_code=status.HTTP_401_UNAUTHORIZED,
                 detail="Invalid or expired token",
-                headers={"WWW-Authenticate":"Bearer"}
+                headers={"WWW-Authenticate": "Bearer"}
             )
-        return {"user":user_response.user,"token":token}
+        return {"user": user_response.user, "token": token}
     except HTTPException:
         raise
     except Exception as e:
@@ -30,10 +29,14 @@ def get_current_user(
 
 def get_supabase_client(current_user=Depends(get_current_user)):
     token = current_user["token"]
-    supabase.postgrest.session.headers.update({
-        "Authorization": f"Bearer {token}"
-    })
-
-    return {"db":supabase , "user":current_user["user"]}
     
-    
+    user_client = create_client(
+        settings.SUPABASE_URL,
+        settings.SUPABASE_KEY,
+        options=ClientOptions(
+            headers={"Authorization": f"Bearer {token}"},
+            auto_refresh_token=False,
+            persist_session=False
+        )
+    )
+    return {"db": user_client, "user": current_user["user"]}
--- app/core/supabase.py
+++ app/core/supabase.py
@@ -1,12 +1,19 @@
-from supabase import create_client,Client
+from supabase import create_client, Client, ClientOptions
 from app.core.config import settings
+
+custom_options = ClientOptions(
+    auto_refresh_token=False, 
+    persist_session=False      
+)
 
 supabase: Client = create_client(
     supabase_url=settings.SUPABASE_URL,
-    supabase_key=settings.SUPABASE_KEY
+    supabase_key=settings.SUPABASE_KEY,
+    options=custom_options
 )
 
-supabase_admin : Client = create_client(
+supabase_admin: Client = create_client(
     supabase_url=settings.SUPABASE_URL,
-    supabase_key=settings.SUPABASE_KEY
+    supabase_key=settings.SUPABASE_KEY,
+    options=custom_options
 )
